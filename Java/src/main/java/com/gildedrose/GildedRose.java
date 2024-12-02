ac3090c2aca1232edddd99b312a0376044b28b81
varvara <106087742+okyavarvara@users.noreply.github.com>
2024-12-02 19:03:18 +0200

    замінила масив на List<Item>

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,13 +1,13 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    List<Item> items;
 
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-    public GildedRose(Item[] items) {
+    public GildedRose(List<Item> items) {
         this.items = items;
     }
 
